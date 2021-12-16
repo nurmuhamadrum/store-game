316959cb93a567e65b1483a16d5340d528a9428b
nurmuhamadrum <nurmuhamadrum@gmail.com>
2021-12-16 15:44:38 +0700

    #create components and organisme

---
+++ components/organisme/NavBar/Menu.tsx
@@ -0,0 +1,27 @@
+import cx from 'classnames';
+import Link from 'next/link';
+
+interface MenuProps {
+    title: string; // if use "? on title?, props is not requiered"
+    active?: boolean;
+    href: string;
+}
+
+export default function Menu(props: Partial<MenuProps>) {
+    const { title, active, href = '/' } = props;
+
+    const classTitle = cx({
+        'nav-link': true,
+        'active': active
+    })
+
+    return (
+        <li className="nav-item my-auto">
+            <Link href={href}>
+                <a className={classTitle} aria-current="page">
+                    {title}
+                </a>
+            </Link>
+        </li>
+    )
+}
